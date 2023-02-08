2dabb2485084ee373057cc07c87fb144518308a0
Zeddzuruzuranda <raphomelvo@gmail.com>
2023-02-08 12:05:33 +0300

    assignment complete

--- methods.rb
+++ methods.rb
@@ -1,1 +1,31 @@
 # Your code here!
+def greet_programmer
+    puts "Hello, programmer!" 
+end
+
+
+def greet(name)
+    puts "Hello, #{name}!"
+end
+
+greet("grace")
+
+def greet_with_default (name="programmer")
+    puts "Hello, #{name}!"
+end
+greet_with_default
+
+def add (numb1,numb2)
+   return numb1 + numb2
+
+end
+add(2,5)
+
+def halve(num3)
+    if num3.class != Integer
+        return nil
+    end
+    return num3/2
+end
+halve(28)
+
